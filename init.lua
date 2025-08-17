72e907ae9478b5cb7f47c0a2e20e35f6115b7b15
Franke Burgarino <frankeburgarino@gmail.com>
2025-08-17 01:22:30 -0500

    switch from init.vim to init.lua

---
+++ init.lua
@@ -0,0 +1,9 @@
+vim.cmd("set number")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set list")
+vim.opt.listchars = {
+	eol = "¬",
+	tab = "  ▸",
+	trail = "·",
+}
--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@ fi
 
 if [ ! -d "$HOME/.config/nvim" ]; then
 	mkdir "$HOME/.config/nvim"
-elif [ -f "$HOME/.config/nvim/init.vim" ]; then
-	rm "$HOME/.config/nvim/init.vim"
+elif [ -f "$HOME/.config/nvim/init.lua" ]; then
+	rm "$HOME/.config/nvim/init.lua"
 fi
 
 ln -s "$HOME/.dotfiles/bashrc" "$HOME/.bashrc"
 
 ln -s "$HOME/.dotfiles/bash_aliases" "$HOME/.bash_aliases"
 
-ln -s "$HOME/.dotfiles/init.vim" "$HOME/.config/nvim/init.vim"
+ln -s "$HOME/.dotfiles/init.lua" "$HOME/.config/nvim/init.lua"
