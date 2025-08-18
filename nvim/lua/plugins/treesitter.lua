45689d75cd635a1de3393224103c56043ede06cf
Franke Burgarino <frankeburgarino@gmail.com>
2025-08-18 01:18:08 -0500

    add bash to treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		config = function()
 			local config = require("nvim-treesitter.configs")
 			config.setup({
-				ensure_installed = { "cpp", "css", "html", "javascript", "lua", "php", "toml", "vim", "yaml" },
+				ensure_installed = { "bash", "cpp", "css", "html", "javascript", "lua", "php", "toml", "vim", "yaml" },
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
