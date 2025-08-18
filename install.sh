c428dcdb0f93ff7bffcc31b56c4ab9c23248c30e
Franke Burgarino <frankeburgarino@gmail.com>
2025-08-18 01:13:18 -0500

    update install.sh to link entire nvim dir

--- install.sh
+++ install.sh
@@ -8,14 +8,12 @@ if [ -f "$HOME/.bash_aliases" ]; then
 	rm "$HOME/.bash_aliases"
 fi
 
-if [ ! -d "$HOME/.config/nvim" ]; then
-	mkdir "$HOME/.config/nvim"
-elif [ -f "$HOME/.config/nvim/init.lua" ]; then
-	rm "$HOME/.config/nvim/init.lua"
+if [ -d "$HOME/.config/nvim" ]; then
+	rm -r "$HOME/.config/nvim"
 fi
 
 ln -s "$HOME/.dotfiles/bashrc" "$HOME/.bashrc"
 
 ln -s "$HOME/.dotfiles/bash_aliases" "$HOME/.bash_aliases"
 
-ln -s "$HOME/.dotfiles/init.lua" "$HOME/.config/nvim/init.lua"
+ln -s "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
